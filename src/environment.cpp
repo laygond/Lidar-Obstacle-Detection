561fa5043560b5cf727fd96341ad68d992f52f72
laygond <laygond_bryan@hotmail.com>
2020-08-14 01:54:23 -0400

    :sparkles: city block complete

--- src/environment.cpp
+++ src/environment.cpp
@@ -7,8 +7,9 @@
 #include "sensors/lidar.h"
 #include "render/render.h"
 #include "processPointClouds.h"
-// using templates for processPointClouds so also include .cpp to help linker
-#include "processPointClouds.cpp"
+#include "processPointClouds.cpp" // using templates so also include .cpp to help linker
+#include <random>
+
 
 
 // ----------------------------------------------------
@@ -92,40 +93,41 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
 // ----------------------------------------------------
 
 /**
- * Load, Filter, Segment, Cluster, Display in Viewer (XYZI Lidar PCD)
+ * Filter, Segment, Cluster, Display in Viewer (XYZI Lidar PCD)
  */
-void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer)
+void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointClouds<pcl::PointXYZI>* pointProcessorI, const pcl::PointCloud<pcl::PointXYZI>::Ptr& inputCloud)
 {
-    //Create point processor
-    ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
-
-    // Read/Load Point Cloud Data
-    pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud = pointProcessorI->loadPcd("../src/sensors/data/pcd/data_1/0000000000.pcd");
+    // Input File (View)
     //renderPointCloud(viewer,inputCloud,"inputCloud");   // if the color is not specified in the renderPointCloud function argument, it will default to using the intensity color coding.
 
     // Filter
     float voxelCubeSize = .2; //[m]
-    Eigen::Vector4f cropMinPoint(-15.0, -5  , -4  , 1.0);
-    Eigen::Vector4f cropMaxPoint( 30  ,  8  ,  4  , 1.0);
+    Eigen::Vector4f cropMinPoint(-15.0, -5  , -2  , 1.0);
+    Eigen::Vector4f cropMaxPoint( 30  ,  8  ,  1  , 1.0);
     pcl::PointCloud<pcl::PointXYZI>::Ptr filterCloud = pointProcessorI->FilterCloud(inputCloud, voxelCubeSize, cropMinPoint, cropMaxPoint);
     //renderPointCloud(viewer,filterCloud,"filterCloud");
 
     // Segment Obstacles and Plane
     std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr> segmentCloud = pointProcessorI->SegmentPlane(filterCloud, 100, 0.2);
-    //renderPointCloud(viewer,segmentCloud.first,"planeCloud",Color(1,0,0));
+    renderPointCloud(viewer,segmentCloud.first,"planeCloud",Color(1,0,1));
     //renderPointCloud(viewer,segmentCloud.second,"obstCloud",Color(0,1,0));
 
+    // Create Color Generator
+    std::default_random_engine gen;
+    std::uniform_real_distribution<float> dist_R(0, 1);
+    std::uniform_real_distribution<float> dist_G(0, 1);
+    std::uniform_real_distribution<float> dist_B(0, 1);
+
     //Cluster points in obstacle cloud to assign boxes
-    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(segmentCloud.second, 2.0, 3, 600);
+    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(segmentCloud.second, 0.53, 10, 500);
     int clusterId = 0;
-    std::vector<Color> colors = {Color(1,0,0), Color(0,1,0), Color(0,0,1), Color(1,0,1), Color(0,1,1), Color(1,1,1), Color(1,1,0), Color(0,.5,0.8), Color(0.5,0,1), Color(0.5,0.5,1)};
     for(pcl::PointCloud<pcl::PointXYZI>::Ptr cluster : cloudClusters)
     {
         std::cout << "cluster size ";
         pointProcessorI->numPoints(cluster);
-        renderPointCloud(viewer,cluster,"obstCloud"+std::to_string(clusterId),colors[clusterId]);
-        //Box box = pointProcessor.BoundingBox(cluster);
-        //renderBox(viewer,box,clusterId);        
+        renderPointCloud(viewer,cluster,"obstCloud"+std::to_string(clusterId),Color(dist_R(gen),dist_G(gen),dist_B(gen)));
+        Box box = pointProcessorI->BoundingBox(cluster);
+        renderBox(viewer,box,clusterId);        
         ++clusterId;
     }
 }
@@ -170,12 +172,60 @@ int main (int argc, char** argv)
     CameraAngle setAngle = XY;
     initCamera(setAngle, viewer);
 
-    // Render in Viewer
-    //simpleHighway(viewer); // Uses simulated Point Cloud Data
-    cityBlock(viewer);       // Uses real Point Cloud Data
 
+    /* 
+    // -------- Simple Highway -------------
+    simpleHighway(viewer); // Uses simulated Point Cloud Data
     while (!viewer->wasStopped ())
     {
-        viewer->spinOnce ();
-    } 
+        viewer->spinOnce (); // controls the frame rate
+    }
+    */ 
+
+
+    /* 
+    // -------- City Block Single .pcd ------------
+    // Create point processor
+    ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
+
+    // Load Point Cloud Data
+    pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud = pointProcessorI->loadPcd("../src/sensors/data/pcd/data_1/0000000000.pcd");
+    
+    // Process Obstacle Detection
+    cityBlock(viewer, pointProcessorI, inputCloud); // process only once     
+    while (!viewer->wasStopped ())
+    {
+        viewer->spinOnce (); // controls the frame rate
+    }
+    */ 
+
+
+    // ------ City Block Stream Multiple .pcd --------
+    // Create point processor
+    ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
+
+    // Create stream object
+    std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_1");
+    auto streamIterator = stream.begin();
+
+    // Create cloud to store iterations of stream
+    pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud;
+
+    // Run Stream
+    while (!viewer->wasStopped ())
+    {
+        // Clear viewer
+        viewer->removeAllPointClouds();
+        viewer->removeAllShapes();
+
+        // Load pcd and run obstacle detection
+        inputCloud = pointProcessorI->loadPcd((*streamIterator).string());
+        cityBlock(viewer, pointProcessorI, inputCloud);
+
+        streamIterator++;
+        if(streamIterator == stream.end())
+            streamIterator = stream.begin();
+
+        viewer->spinOnce (); // controls the frame rate
+    }
 }
